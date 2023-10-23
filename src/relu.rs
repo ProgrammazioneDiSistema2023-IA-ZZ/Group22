506020e6db209084b05b02388974409362fc0428
RAFFAELE VIOLA s309063 <s309063@studenti.polito.it>
2023-10-23 18:43:07 +0200

    added parallelization to Relu

--- src/relu.rs
+++ src/relu.rs
@@ -1,6 +1,7 @@
 use ndarray::{Array1, Array4, Array, arr1, Shape, Dim};
 use crate::operations::{Compute, Input, Output};
 use crate::onnx_proto3::{AttributeProto, NodeProto};
+use ndarray::parallel;
 
 #[derive(Clone, Debug)]
 pub struct Relu{
@@ -19,21 +20,21 @@ impl Relu{
 impl Compute for Relu {
     fn compute(&mut self, inputs: Input) -> Output {
         match inputs {
-            Input::Tensor1(array) => {
-                let result = array.mapv(|x| if x > 0.0 { x } else { 0.0 });
-                Output::Tensor1(result)
+            Input::Tensor1(mut array) => {
+                array.par_mapv_inplace(|x| if x > 0.0 { x } else { 0.0 });
+                Output::Tensor1(array)
             }
-            Input::Tensor2(array) => {
-                let result = array.mapv(|x| if x > 0.0 { x } else { 0.0 });
-                Output::Tensor2(result)
+            Input::Tensor2(mut array) => {
+                array.par_mapv_inplace(|x| if x > 0.0 { x } else { 0.0 });
+                Output::Tensor2(array)
             }
-            Input::Tensor3(array) => {
-                let result = array.mapv(|x| if x > 0.0 { x } else { 0.0 });
-                Output::Tensor3(result)
+            Input::Tensor3(mut array) => {
+                array.par_mapv_inplace(|x| if x > 0.0 { x } else { 0.0 });
+                Output::Tensor3(array)
             }
-            Input::Tensor32(array) => {
-                let result = array.mapv(|x| if x > 0.0 { x } else { 0.0 });
-                Output::Tensor32(result)
+            Input::Tensor32(mut array) => {
+                array.par_mapv_inplace(|x| if x > 0.0 { x } else { 0.0 });
+                Output::Tensor32(array)
             }
             _ => panic!("Wrong input"),
         }
